bbff1e596ed0e680c87c072f8d0055485df8f2ec
Minionguyjpro <66115754+Minionguyjpro@users.noreply.github.com>
2021-12-23 15:11:07 +0100

    Add Shutdown EXE files

--- Batch-Files/Shutdown/Reboot.bat
+++ Batch-Files/Shutdown/Reboot.bat
@@ -1,2 +1,35 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAjk
+::fBw5plQjdCyDJGyX8VAjFBNDRxaXAE+1EbsQ5+n//NaUqlgJUO0odsHS2bvu
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSDk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+IeA==
+::cxY6rQJ7JhzQF1fEqQJhZksaHErSXA==
+::ZQ05rAF9IBncCkqN+0xwdVsFAlTMbCXqZg==
+::ZQ05rAF9IAHYFVzEqQIDIA5ASAuVMgs=
+::eg0/rx1wNQPfEVWB+kM9LVsJDDeKKX++CaISiA==
+::fBEirQZwNQPfEVWB+kM9LVsJDDeKKX++CaISiA==
+::cRolqwZ3JBvQF1fEqQIDIA5ACxfCfFyzCLET//u76+OVp04JWPt/fIDM1NQ=
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCyDJGyX8VAjFBNDRxaXAE+1EbsQ5+n//NaysUgbQe5yT4bV+LWaI+gW+XnGdIIu3jd8kccJHydrdge/agAjp1Ja5Ta5BM+JpwvuXUbH41M1ew==
+::YB416Ek+Zm8=
+::
+::
+::978f952a14a936cc963da21a135fa983
 : This script reboots Windows directly and reboots like normal.
 shutdown /r /t 00
